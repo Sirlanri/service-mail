9dbd7ec3c01ff6bd76329c134bae02e2a43299c5
深蓝 <mail@ri-co.cn>
2022-04-02 09:12:06 +0800

    初始化iris框架

--- main.go
+++ main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"github.com/Sirlanri/service-mail/server"
+	"github.com/kataras/iris/v12"
+)
+
+func main() {
+	app := iris.New()
+	app.OnErrorCode(iris.StatusNotFound, server.NotFound)
+	mail := app.Party("/mail").AllowMethods(iris.MethodOptions)
+	{
+		mail.Post("/send")
+	}
+}
